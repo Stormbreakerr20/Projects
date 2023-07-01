ce7f052e1a6c28f5e81763361c46a7aa806fa1d0
Stormbreaker20 <luvsharma.del@gmail.com>
2023-07-01 23:35:30 +0530

    RandomColGen

--- StickyNavbar/nav.js
+++ StickyNavbar/nav.js
@@ -5,7 +5,7 @@ const bottom = document.querySelector('.bottom')
 window.addEventListener('scroll', ()=>{
     console.log(window.scrollY);
 
-    if(window.scrollY> bottom.offsetTop - 50 - nav.offsetHeight){
+    if(window.scrollY> bottom.offsetTop  - nav.offsetHeight){
         nav.classList.add('active');
     }
     else{
